4aeeeac7b53cbc923c95b6ebcc5c9819f5da8675
Bhargava Rao <suthrayebhargavarao@gmail.com>
2026-04-01 13:05:23 +0530

    Fix all page components

---
+++ app/admissions/process/page.js
@@ -0,0 +1,8 @@
+export default function ProcessPage() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Admission Process</h1>
+      <p>Details coming soon...</p>
+    </div>
+  );
+}
