1f7af0d1d4563dc9aa44d8d8919949c974416a83
Bhargava Rao <suthrayebhargavarao@gmail.com>
2026-04-01 12:54:34 +0530

    Fix vision mission page

---
+++ app/about/vision-mission/page.js
@@ -0,0 +1,8 @@
+export default function VisionMissionPage() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Vision & Mission</h1>
+      <p>Welcome to Vision and Mission Page</p>
+    </div>
+  );
+}
